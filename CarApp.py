1f01958a691743891ec4a62cb9bf8b094447093d
Mansi Ingale <ingalemansi70@gmail.com>
2026-07-18 03:59:08 +0530

    Loaded the all pkl files

--- CarApp.py
+++ CarApp.py
@@ -1,4 +1,8 @@
 # Q1.
 import streamlit as st
 import pandas as pd
-import joblib
+import joblib 
+# Q2.
+model = joblib.load('LR_ford_car.pkl')
+scaler = joblib.load('scaler.pkl')
+encoded_columns = joblib.load('columns.pkl')
