81f006bbbe55a43c543f8ae490f96e74df4d01a1
Mansi Ingale <ingalemansi70@gmail.com>
2026-07-18 04:09:20 +0530

    Numerical Input Fields

--- CarApp.py
+++ CarApp.py
@@ -15,3 +15,14 @@ st.set_page_config(
 st.title("🚗 Ford Car Price Predictor")
 st.write("Enter the car details below to predict its selling price.")
 st.divider()
+
+# Q5.
+st.subheader("📊 Numerical Inputs")
+col1, col2 = st.columns(2)
+with col1:
+    year = st.number_input("Manufacturing Year", min_value=1990, max_value=2026, value=2018, step=1)
+    mileage = st.number_input("Mileage", min_value=0, max_value=500000, value=40000, step=1000)
+    tax = st.number_input("Road Tax", min_value=0, max_value=2000, value=150, step=10)
+with col2:
+    mpg = st.number_input("MPG", min_value=5.0, max_value=100.0, value=45.0, step=0.1)
+    engine_size = st.number_input("Engine Size", min_value=0.5, max_value=6.0, value=1.5, step=0.1)
