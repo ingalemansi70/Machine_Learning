6071aedcde28d252f53c7d5abb544ba8c2053b76
Mansi Ingale <ingalemansi70@gmail.com>
2026-07-18 04:05:39 +0530

    Page configuration

--- CarApp.py
+++ CarApp.py
@@ -6,3 +6,8 @@ import joblib
 model = joblib.load('LR_ford_car.pkl')
 scaler = joblib.load('scaler.pkl')
 encoded_columns = joblib.load('columns.pkl')
+# Q3.
+st.set_page_config(
+    page_title="Ford Car Price Predictor",
+    layout="centered"
+)
